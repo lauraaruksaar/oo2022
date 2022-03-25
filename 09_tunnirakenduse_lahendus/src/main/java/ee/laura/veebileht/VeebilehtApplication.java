10320836bfa7834b1152c76f9c41b8a07ced6c15
Laura Aruksaar <lauraaruksaar@users.noreply.github.com>
2022-03-25 15:31:49 +0200

    tunni lõpp

---
+++ 09_tunnirakenduse_lahendus/src/main/java/ee/laura/veebileht/VeebilehtApplication.java
@@ -0,0 +1,13 @@
+package ee.laura.veebileht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeebilehtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeebilehtApplication.class, args);
+    }
+
+}
---
+++ 09_tunnirakenduse_lahendus/src/test/java/ee/laura/veebileht/VeebilehtApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.laura.veebileht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeebilehtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
